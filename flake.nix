fc586ea33f4a365829d2ca8c06238a60f86025a4
Clemens Horn <clemens.horn@mni.thm.de>
2025-03-24 10:12:14 +0100

    chore(nix): update

--- flake.nix
+++ flake.nix
@@ -1,10 +1,19 @@
 {
+  nixConfig = {
+    extra-substituters = ["https://nix-gaming.cachix.org"];
+    extra-trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+  };
   inputs = {
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
     nix-gaming = {
       url = "github:fufexan/nix-gaming";
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
     };
   };
   outputs = {
